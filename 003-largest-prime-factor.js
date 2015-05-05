db89277c9e35b902ae6a109fcd7af77d7c722b1a
m-arnold <michaelaarnold@gmail.com>
2015-05-05 16:29:05 -0700

    implemented better.

--- 003-largest-prime-factor.js
+++ 003-largest-prime-factor.js
@@ -1,22 +1,18 @@
 var num = 600851475143;
 
-var isPrime = function (x) {
-  for (var i = 2; i <= Math.ceil(x / 2); i++) {
-    if (x % i === 0) {
-      return false;
+var primeFactors = function (n) {
+  var factors = [];
+
+  for (var i = 2; i <= n; i++) {
+    while (n % i === 0) {
+      factors.push(i);
+      n = n / i;
     }
   }
-  return true;
+
+  return factors;
 };
 
-var largestPrimeFactor = function (n) {
-  for (var i = 2; i < Math.ceil(n / 2); i++) {
-    if (n % i === 0 && isPrime(i)) {
-      largestPrimeFactor(i);
-    }
-  }
-  console.log("prime factor: " + n);
-  return n;
-};
+var result = primeFactors(num);
 
-largestPrimeFactor(num);
+console.log(result[result.length - 1]);
